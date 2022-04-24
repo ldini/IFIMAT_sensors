27b1f70dec68556218ad9d46f13dd680fa3476b3
Leandro <leandro.david.dini@gmail.com>
2022-04-24 13:49:45 -0300

    server agregado

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
-const { SerialPort, ReadlineParser } = require('serialport')
-const port = new SerialPort({ path:'COM9', baudRate:9600 })
-const parser = new ReadlineParser()
-port.pipe(parser)
-parser.on('data', console.log)
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
 
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+
+const { SerialPort, ReadlineParser } = require('serialport') // Serial port
+const port = new SerialPort({ path:'COM9', baudRate:9600 }) // config serial port
+const parser = new ReadlineParser() // nuevo puerto
+
+port.on('open', () => { //preguntamos si esta conectado al puerto
+    console.log('Puerto Conectado')
+})
+
+port.pipe(parser)
+
+parser.on('data', console.log) // mostramos los datos
+
+port.on('err',console.log) // mostramos error
+
+server.listen(3000, () => {
+    console.log('server on port ',3000)
+})
--- server/index.ts
+++
@@ -1,4 +0,0 @@
-import { SerialPort } from 'serialport';
-
-const serialport = new SerialPort({ path: 'COM9', baudRate: 9600 });
-serialport.write('ROBOT POWER ON');
