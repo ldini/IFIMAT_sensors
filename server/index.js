ea045d1389462b0d342562959438c35ce1de1693
Leandro <leandro.david.dini@gmail.com>
2022-04-24 14:13:08 -0300

    aviso de cliente conectado

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+io.on('connection', function(socket){
+    console.log('un nuevo cliente conectado');
+})
+
+app.get('/', (req,res,next) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 const { SerialPort, ReadlineParser } = require('serialport') // Serial port
 const port = new SerialPort({ path:'COM9', baudRate:9600 }) // config serial port
@@ -18,10 +25,16 @@ port.on('open', () => { //preguntamos si esta conectado al puerto
 
 port.pipe(parser)
 
-parser.on('data', console.log) // mostramos los datos
+//parser.on('data', console.log) // mostramos los datos simplificado
+parser.on('data',function(data){
+    console.log(data);
+    io.emit('arduino:data',{
+        value: data
+    })
+})
 
 port.on('err',console.log) // mostramos error
 
 server.listen(3000, () => {
-    console.log('server on port ',3000)
+    console.log('server on port ',3000);
 })
