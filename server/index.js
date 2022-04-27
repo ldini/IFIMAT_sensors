09550ae2813232854b91621844a264db0c25f8c2
Leandro <leandro.david.dini@gmail.com>
2022-04-27 17:35:29 -0300

    cambio de muestreo de datos

--- server/index.js
+++ server/index.js
@@ -15,6 +15,11 @@ app.get('/', (req,res,next) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/chartjs-plugin-zoom.min.js', (req,res,next) => {
+    res.sendFile(__dirname + '/chartjs-plugin-zoom.min.js');
+});
+
+
 const { SerialPort, ReadlineParser } = require('serialport') // Serial port
 const port = new SerialPort({ path:'COM9', baudRate:9600 }) // config serial port
 const parser = new ReadlineParser() // nuevo puerto
@@ -25,12 +30,26 @@ port.on('open', () => { //preguntamos si esta conectado al puerto
 
 port.pipe(parser)
 
+let data = [];
+
+
+app.get('/export', (req,res,next) => {
+    res.type('text/csv')
+    let str = 'tiempo,sensor1,sensor2,sensor3,sensor4\n'
+    for (let index = 0; index < data.length; index++) {
+        const e = data[index];
+        str += `${e.time},${e.sensor1},${e.sensor2},${e.sensor3},${e.sensor4}\n`
+    }
+    res.send(str);
+});
+
 //parser.on('data', console.log) // mostramos los datos simplificado
-parser.on('data',function(data){
-    console.log(JSON.parse(data));
-    io.emit('arduino:data',{
-        value: [JSON.parse(data).sensor1,JSON.parse(data).sensor2,JSON.parse(data).sensor3,JSON.parse(data).sensor4]
-    })
+parser.on('data',function(serialData){
+    let val = JSON.parse(serialData);
+    val.time = data.length;
+    data.push(val);
+    console.log(val);
+    io.emit('arduino:data', data.slice(-10))
 })
 
 port.on('err',console.log) // mostramos error
